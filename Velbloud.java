7fb3167cee17787f91c1e5cfbecc077570ea540c
Martin Šimůnek <simunek@oauh.cz>
2024-04-11 11:25:10 +0200

    Úvodní commit
    Založil jsem projekt a vytvořil aktéra Velbloud.

---
+++ Velbloud.java
@@ -0,0 +1,19 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class Velbloud here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Velbloud extends Actor
+{
+    /**
+     * Act - do whatever the Velbloud wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+    }
+}
